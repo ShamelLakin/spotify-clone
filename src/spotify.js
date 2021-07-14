be949582d76762aa30fa16dc49bf47df9aea465c
Shamel Lakin <christlife4u@gmail.com>
2021-07-14 11:00:10 -0400

    spotify scopes

--- src/spotify.js
+++ src/spotify.js
@@ -3,3 +3,13 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 const redirectUri = "http://localhost:3000/";
+
+const clientid = "48eec709cdf6410dba69289ee3f744a7";
+
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
