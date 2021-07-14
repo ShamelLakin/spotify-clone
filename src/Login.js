8f734cecd6069e38b52b3b224fd6f1dc35f49343
Shamel Lakin <christlife4u@gmail.com>
2021-07-14 10:12:51 -0400

    button fix

--- src/Login.js
+++ src/Login.js
@@ -6,7 +6,7 @@ function Login() {
         <div className="login">
             <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="" />
             
-            <button>LOGIN WITH SPOTIFY</button>
+            <a>LOGIN WITH SPOTIFY</a>
         </div>
     )
 }
