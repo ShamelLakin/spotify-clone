e33cd245ce26b7de00af9ab131bfe29acce75552
Shamel Lakin <christlife4u@gmail.com>
2021-08-01 12:49:38 -0400

    body css

--- src/Player.js
+++ src/Player.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./styles/Player.css";
 import Sidebar from "./Sidebar";
 import Body from "./Body";
+import Footer from "./Footer";
 
 function Player({ spotify }) {
   return (
@@ -12,7 +13,7 @@ function Player({ spotify }) {
         <Body />
       </div>
 
-      {/* Footer */}
+      <Footer/>
     </div>
   );
 }
