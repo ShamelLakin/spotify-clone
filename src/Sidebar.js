fa6706f04429bc302f340664454566e2d868b49f
Shamel Lakin <christlife4u@gmail.com>
2021-08-03 20:22:34 -0400

    sidebar icons

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./styles/Sidebar.css";
+import SidebarOption from "./SidebarOption";
 
 function Sidebar() {
   return (
@@ -8,7 +9,10 @@ function Sidebar() {
         className="sidebar__logo"
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         alt=""
-      ></img>
+      />
+      <SidebarOption title="Home" />
+      <SidebarOption title="Search" />
+      <SidebarOption title="Your library" />
     </div>
   );
 }
---
+++ src/SidebarOption.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles/SidebarOption.css";
+
+
+function SidebarOption({ title, Icon }) {
+  return (
+    <div className="sidebarOption">
+      <p>{title}</p>
+
+      
+    </div>
+  );
+}
+
+export default SidebarOption;
