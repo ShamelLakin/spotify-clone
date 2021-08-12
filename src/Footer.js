f2a8760f249957d655f57a7f21a478b38aad3241
Shamel Lakin <christlife4u@gmail.com>
2021-08-12 11:58:01 -0400

    footer_center

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,29 @@
 import React from "react";
 import "./styles/Footer.css";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
 
 function Footer() {
   return (
     <div className="footer">
-      <h1>I am the footer</h1>
+      <div className="footer__left">
+        <p>Album and song details</p>
+      </div>
+
+      <div className="footer__center">
+        <ShuffleIcon className="footer__green" />
+        <SkipPreviousIcon className="footer__icon" />
+        <PlayCircleOutlineIcon fontSize="large" className="footer__icon" />
+        <SkipNextIcon className="footer__icon" />
+        <RepeatIcon className="footer__green" />
+      </div>
+
+      <div className="footer__right">
+        <p>Volume controls</p>
+      </div>
     </div>
   );
 }
