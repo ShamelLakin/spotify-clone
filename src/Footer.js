5d120daf56929642e581dc14f9f7cdc4e3cf6c8b
Shamel Lakin <christlife4u@gmail.com>
2021-08-12 12:22:19 -0400

    footer__right icons

--- src/Footer.js
+++ src/Footer.js
@@ -5,6 +5,9 @@ import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
 import SkipNextIcon from "@material-ui/icons/SkipNext";
 import ShuffleIcon from "@material-ui/icons/Shuffle";
 import RepeatIcon from "@material-ui/icons/Repeat";
+import { Grid, Slider } from "@material-ui/core";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
 
 function Footer() {
   return (
@@ -22,7 +25,17 @@ function Footer() {
       </div>
 
       <div className="footer__right">
-        <p>Volume controls</p>
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider />
+          </Grid>
+        </Grid>
       </div>
     </div>
   );
