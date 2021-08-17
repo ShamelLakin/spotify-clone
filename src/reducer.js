132956fbfbd434cfd2ef11a72ef711ab0f611b11
Shamel Lakin <christlife4u@gmail.com>
2021-08-17 10:06:10 -0400

    datalayerchange

--- src/reducer.js
+++ src/reducer.js
@@ -31,7 +31,7 @@ const reducer = (state, action) => {
     case "SET_DISCOVER_WEEKLY":
       return {
         ...state,
-        taste: action.taste,
+        discover_weekly: action.discover_weekly,
       };
     default:
       return state;
