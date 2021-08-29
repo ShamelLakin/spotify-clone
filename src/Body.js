21c412ad1296f1f7eb5ae6bc06d480b1e5f7e2ac
Shamel Lakin <christlife4u@gmail.com>
2021-08-29 15:19:43 -0400

    change discover weekly

--- src/Body.js
+++ src/Body.js
@@ -14,7 +14,7 @@ function Body({ spotify }) {
         <img src={discover_weekly?.images[0].url} alt="" />
         <div className="body__infoText">
           <strong>PLAYLIST</strong>
-          <h2>Discover Weekly</h2>
+          <h2>Taste</h2>
           <p>{discover_weekly?.description}</p>
         </div>
       </div>
