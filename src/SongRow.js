18e91c669ff7f7c3eea01d418a042157f759b1ca
Shamel Lakin <christlife4u@gmail.com>
2021-08-30 12:10:40 -0400

    songrow css

--- src/SongRow.js
+++ src/SongRow.js
@@ -1,13 +1,16 @@
 import React from "react";
+import './styles/SongRow.css';
 
 function SongRow({ track = "test" }) {
   return (
     <div className="songRow">
-      <img src="" alt="" />
+      <img className="songRow__album" src="{track.album.images[0].url}" alt="" />
       <div className="songRow__info">
         <h1>{track.name}</h1>
-        <p>{track.artist.map((artist) => artist.name)}</p>
-        {track.album.name}
+        <p>
+          {track.artist.map((artist) => artist.name).join(", ")}
+          {track.album.name}
+        </p>
       </div>
     </div>
   );
