9828d208356f246b70ce04dd9c8166b9f26748e9
Shamel Lakin <christlife4u@gmail.com>
2021-08-30 12:03:17 -0400

    map through artist

--- src/SongRow.js
+++ src/SongRow.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from "react";
 
-function SongRow( {track = "test"}) {
-    return 
+function SongRow({ track = "test" }) {
+  return (
     <div className="songRow">
-    <img src="" alt=""/>
+      <img src="" alt="" />
       <div className="songRow__info">
-
+        <h1>{track.name}</h1>
+        <p>{track.artist.map((artist) => artist.name)}</p>
+        {track.album.name}
       </div>
-    </div>;
-    
+    </div>
+  );
 }
 
-export default SongRow
+export default SongRow;
